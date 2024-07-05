f9821d86d781337e1f02687c0542e5e57ae6e850
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2024-07-06 00:38:19 +0300

    init: initialize react + primereact app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Button } from 'primereact/button';
+
+
+
+function App() {
+  return (
+    <>
+      <Button
+        label='test'
+        size='small'
+      />
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+// primereact
+import 'primereact/resources/themes/lara-light-teal/theme.css';
+// primeflex
+import 'primeflex/primeflex.css';
+// primeicons
+import 'primeicons/primeicons.css';
+
+// style
+import './assets/styles/main.scss';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
